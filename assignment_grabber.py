8cf338346bdd4745a46337ededf014b861aef7b4
Vince Cooley <vince.r.cooley@gmail.com>
2015-09-21 21:50:36 -0500

    gets students links using bs4

--- assignment_grabber.py
+++ assignment_grabber.py
@@ -1,13 +1,14 @@
-__author__ = 'Vince'
 
 from bs4 import BeautifulSoup
 import keyring
 import requests
 
 
+username = "vcooley"
+
+
 def login(s):
     keys = keyring.get_keyring()
-    username = "vcooley"
     password = keys.get_password("pythonanywhere", username)
 
     url = "https://www.pythonanywhere.com/login/"
@@ -21,7 +22,25 @@ def login(s):
                }
     r = s.post(url, data=payload, headers=header)
 
-soup =
+
+def get_students(s):
+    """
+    Gets a list of links to students pages
+    :param s:
+    :return student href list:
+    """
+    url = "https://www.pythonanywhere.com/user/{}/consoles".format(username)
+    r = s.get(url)
+    soup = BeautifulSoup(r.text)
+    students_li_items = soup.find_all('li', role='presentation')
+    student_links = []
+    for li in students_li_items:
+        link = li.a['href']
+        student_links.append(link)
+    return student_links
+
+
+
 
 def main():
     with requests.Session() as s:
