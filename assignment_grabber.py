17133668924a496e11e758a143cefa604cd6b46b
Vince <vince.r.cooley@gmail.com>
2015-09-22 16:20:24 -0500

    Gets user names and gets files. TODO: create path and save files

--- assignment_grabber.py
+++ assignment_grabber.py
@@ -1,22 +1,23 @@
 
 from bs4 import BeautifulSoup
 import keyring
+import os
 import requests
 
 
-username = "vcooley"
+teacher_username = "vcooley"
 
 
 def login(s):
     keys = keyring.get_keyring()
-    password = keys.get_password("pythonanywhere", username)
+    password = keys.get_password("pythonanywhere", teacher_username)
 
     url = "https://www.pythonanywhere.com/login/"
     header = {"Referer": url}
     s.get(url)
     csrftoken = s.cookies['csrftoken']
 
-    payload = {"username": username,
+    payload = {"username": teacher_username,
                "password": password,
                "csrfmiddlewaretoken": csrftoken
                }
@@ -29,16 +30,55 @@ def get_students(s):
     :param s:
     :return student href list:
     """
-    url = "https://www.pythonanywhere.com/user/{}/consoles".format(username)
+    url = "https://www.pythonanywhere.com/user/{}/consoles".format(teacher_username)
     r = s.get(url)
     soup = BeautifulSoup(r.text)
     students_li_items = soup.find_all('li', role='presentation')
-    student_links = []
+    student_usernames = []
     for li in students_li_items:
         link = li.a['href']
-        student_links.append(link)
-    return student_links
+        user = link[6:-1]
+        student_usernames.append(user)
+    return student_usernames
 
+
+def get_file(s, user, filename, path='/', root=None):
+    """
+    Takes a session object, a username, a filename, the path to the file,
+    and the root if it is not the standard path the user finds when navigating to his files tab.
+    Returns a file if it is found or None if it is not found.
+    :param s:
+    :param user:
+    :param filename:
+    :param path:
+    :param root:
+    :return file or None:
+    """
+    if root is None:
+        root = "/home/{}".format(user)
+    url = "https://www.pythonanywhere.com/user/{user}/files{root}{path}{filename}".format(user=user,
+                                                                                          root=root,
+                                                                                          path=path,
+                                                                                          filename=filename
+                                                                                          )
+    r = s.get(url)
+    if r.status_code == 200:
+        return r.text
+    else:
+        return None
+
+
+def save_assignments(s, assignment_name, user_list, local_user, file_path=None):
+    """
+    :param s: request.Session
+    :param assignment_name: str
+    :param user_list: list
+    :return: None
+    """
+    if not file_path:
+        file_path = "C://Users/{}/{}".format(local_user, assignment_name)
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
 
 
 
