c848a3c4380486095735681a60f6cec39085ebfa
Vince Cooley <vince.r.cooley@gmail.com>
2015-09-22 20:41:17 -0500

    fully functional. Prompts for assignment_name and assignment path. Assumes assignment will be named exactly assignment_name.py and the file will be found at exactly the specified path

--- assignment_grabber.py
+++ assignment_grabber.py
@@ -68,20 +68,42 @@ def get_file(s, user, filename, path='/', root=None):
         return None
 
 
-def save_assignments(s, assignment_name, user_list, local_user, file_path=None):
+def save_assignments(s, assignment_name, user_list, assignment_path=None, file_path=None):
     """
     :param s: request.Session
     :param assignment_name: str
     :param user_list: list
     :return: None
     """
+    if not assignment_path:
+        assignment_path = '/'
+    filename = assignment_name + ".py"
+    local_user = os.getenv("USERNAME")
     if not file_path:
-        file_path = "C://Users/{}/{}".format(local_user, assignment_name)
+        file_path = "C://Users/{}/Documents/CompSci/{}".format(local_user, assignment_name)
     if not os.path.exists(file_path):
         os.makedirs(file_path)
+    for student in user_list:
+        file_as_str = get_file(s, student, filename, assignment_path)
+        student_file_path = file_path + "/{}.py".format(student)
+        if file_as_str:
+            with open(student_file_path, "w") as assignment:
+                assignment.write(file_as_str)
 
+
+def prompt_for_info():
+    assignment_name = input("Enter assignment name:")
+    assignment_path = input("Enter assignment path (e.g. /homework/ or /practice/:")
+    return assignment_name, assignment_path
 
 
 def main():
     with requests.Session() as s:
         login(s)
+        students = get_students(s)
+        assignment, path = prompt_for_info()
+        save_assignments(s, assignment, students)
+
+
+if __name__ == "__main__":
+    main()
